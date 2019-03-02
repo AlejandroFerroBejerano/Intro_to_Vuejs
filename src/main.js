03ebeb22e538fb38c28d54d09c310b41ed8e5ec3
Alejandro Ferro <alejandroferro.1988@gmail.com>
2019-03-02 17:42:43 +0100

    Added event Handling for cart button

--- src/main.js
+++ src/main.js
@@ -11,13 +11,27 @@ var app = new Vue({
     variants: [
       {
         variantId: 2234,
-        variantColor: 'green'
+        variantColor: 'green',
+        variantImage: '../img/vmSocks-green-onWhite.jpg'
       },
       {
         variantId: 2235,
-        variantColor: 'blue'
+        variantColor: 'blue',
+        variantImage: '../img/vmSocks-blue-onWhite.jpg'
       }
     ],
-    sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
+    sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+    cart: 0
+  },
+  methods: {
+    addToCart(){
+      this.cart += 1
+    },
+    updateProduct(variantImage){
+      this.image = variantImage
+    },
+    removeFromCart(){
+      this.cart -= 1
+    }
   }
 });

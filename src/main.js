e2e495e3caa906a38217fd0fcc083b88cd5b1dc8
Alejandro Ferro <alejandroferro.1988@gmail.com>
2019-03-02 16:51:45 +0100

    Conditional rendering parts added

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 var app = new Vue({
   el: '#app',
   data: {
-    product: 'boots'
+    product: 'Sooks',
+    image: '../img/vmSocks-green-onWhite.jpg',
+    description: 'A pair of warm, fuzzy socks',
+    inventory: 10,
+    link: 'https://goo.gl/s8ca8G'
   }
-})
+});

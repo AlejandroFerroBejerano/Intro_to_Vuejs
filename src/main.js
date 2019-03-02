906fec53b948835b665ed84a59c05aeac228e309
Alejandro Ferro <alejandroferro.1988@gmail.com>
2019-03-02 17:13:51 +0100

    Added List Rendering

--- src/main.js
+++ src/main.js
@@ -5,6 +5,19 @@ var app = new Vue({
     image: '../img/vmSocks-green-onWhite.jpg',
     description: 'A pair of warm, fuzzy socks',
     inventory: 10,
-    link: 'https://goo.gl/s8ca8G'
+    link: 'https://goo.gl/s8ca8G',
+    inStock: true,
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: 'green'
+      },
+      {
+        variantId: 2235,
+        variantColor: 'blue'
+      }
+    ],
+    sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
   }
 });
